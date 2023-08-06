067151d17b80ea733db2976781f8ff66e5ba7378
Gathan Mahesa <gthn217@gmail.com>
2023-08-06 17:43:53 +0700

    Update : Selection page finished

--- src/ts/routes/selected.ts
+++ src/ts/routes/selected.ts
@@ -1,33 +1,33 @@
 import projects from '../../../public/works.json';
 
 const selected = `
-<div class="content selection w-screen h-screen  flex flex-col gap-16">
-  <header style="transform:translateX(0%)" class="fill-image p-1 w-max h-20 grid grid-flow-col gap-x-2 scroll-auto overflow-visible">
+
+<div class="content selection w-screen h-screen grid grid-rows-[auto_1fr] gap-y-16">
+  <header style="transform:translateX(0%)" class="fill-image p-1 w-max min-h-20 grid grid-flow-col gap-x-2 scroll-auto overflow-visible">
     ${projects
     .map(
-      (project, index) => `
-      <div class="selection item-container" data-project-id="${index}">
+      (project) => `
+      <div class="selection item-container">
         <img draggable="false" class="selection item-thumbnail" src="${project.src}" alt="${project.alt}"/>
-      </div>
+        </div>
       `
       )
       .join('')}
     ${projects
     .map(
-      (project, index) => `
-      <div class="selection item-container" data-project-id="${index}">
+      (project) => `
+      <div class="selection item-container">
         <img draggable="false" class="selection item-thumbnail" src="${project.src}" alt="${project.alt}"/>
-      </div>
+        </div>
       `
       )
       .join('')}
   </header>
-  <main class="w-screen h-full">
-    <div class="h-full w-96  main-image-container">
-    </div>
-  </main>
-</div>`
+  <div class="relative w-1/3 h-full overflow-hidden main-image-container">
+    <img draggable="false" class="main-image h-full absolute bottom-0 left-0 object-contain" src="/works/beehive.webp" alt="kontol"/>
+  </div>
+</div>
 
-
+`
 
 export default selected
--- src/ts/smoothscrolling.ts
+++ src/ts/smoothscrolling.ts
@@ -1,82 +1,127 @@
+class SmoothScroller {
+  current: number;
+  target: number;
+  isDragging: boolean;
+  startY: number;
+  ease: number;
+  selectionHeader: HTMLHeadElement;
+  itemWidth: HTMLDivElement;
+  maximumX: number;
 
-let current = 0;
-let target = 0;
-let isDragging = false;
-let startY = 0;
-const ease = 0.075;
+  constructor() {
+    this.current = 0;
+    this.target = 0;
+    this.isDragging = false;
+    this.startY = 0;
+    this.ease = 0.075;
 
-function lerp (start:number, end:number, t:number){
-  return start * (1 - t) + (end * t)
-}
+    this.selectionHeader = document.querySelector('.selection > header')!;
+    this.itemWidth = document.querySelector('.selection > header > .item-container')!;
+    this.maximumX = parseFloat(getComputedStyle(this.selectionHeader).width) - parseFloat(getComputedStyle(this.itemWidth).width);
 
-function setTransform(element: HTMLElement | null, change: string) {
-  if (element) {
-    element.style.transform = change;
+    this.smoothScroll = this.smoothScroll.bind(this);
+    this.handleWheel = this.handleWheel.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
+
+    this.init();
   }
-}
 
-function handleSelectionHeaderAppearance() {
-  const selection_header:HTMLHeadElement = document.querySelector('.selection > header')!;
-  const item_width: HTMLDivElement = document.querySelector('.selection > header > .item-container')!
-  const maximum_x:number = parseFloat(getComputedStyle(selection_header).width) - parseFloat(getComputedStyle(item_width).width)
-  
-  function smoothScroll (){
-    current = lerp(current, target, ease)
-    current = parseFloat(current.toFixed(2))
-    setTransform(selection_header, `translate3d(${-current}px, 0 ,0)`);
-    requestAnimationFrame(smoothScroll);
+  lerp(start: number, end: number, t: number) {
+    return start * (1 - t) + (end * t);
   }
 
-  const handleWheel = (e:WheelEvent) => {
-    e.preventDefault()
-    target = Math.min(maximum_x, Math.max(0, target + e.deltaY * 0.5))
+  setTransform(element: HTMLElement | null, change: string) {
+    if (element) {
+      element.style.transform = change;
+    }
   }
 
-  function handleMouseDown(event: MouseEvent) {
-    isDragging = true;
-    startY = event.clientX;
+  smoothScroll() {
+    this.current = this.lerp(this.current, this.target, this.ease);
+    this.current = parseFloat(this.current.toFixed(2));
+    this.setTransform(this.selectionHeader, `translate3d(${-this.current}px, 0 ,0)`);
+    requestAnimationFrame(this.smoothScroll);
+  }
+
+  handleWheel(e: WheelEvent) {
+    e.preventDefault();
+    this.target = Math.min(this.maximumX, Math.max(0, this.target + e.deltaY * 1.5));
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    this.isDragging = true;
+    this.startY = event.clientX;
     document.body.style.cursor = 'grabbing';
   }
-  
-  function handleMouseMove(event: MouseEvent) {
-    if (!isDragging) return;
-    const deltaY = (event.clientX - startY!);
-    target = Math.min(maximum_x, Math.max(0, target - deltaY * 2.5));
-    startY = event.clientX;
+
+  handleMouseMove(event: MouseEvent) {
+    if (!this.isDragging) return;
+    const deltaY = event.clientX - this.startY;
+    this.target = Math.min(this.maximumX, Math.max(0, this.target - deltaY * 2.5));
+    this.startY = event.clientX;
   }
-  
-  function handleMouseUp() {
-    isDragging = false;
+
+  handleMouseUp() {
+    this.isDragging = false;
     document.body.style.cursor = 'default';
   }
 
-  function handleTouchStart(event: TouchEvent) {
-    isDragging = true;
-    startY = event.touches[0].clientX;
+  handleTouchStart(event: TouchEvent) {
+    this.isDragging = true;
+    this.startY = event.touches[0].clientX;
   }
 
-  function handleTouchMove(event: TouchEvent) {
-    if (!isDragging) return;
-    const deltaY = (event.touches[0].clientX - startY!);
-    target = Math.min(maximum_x, Math.max(0, target - deltaY * 2.5));
-    startY = event.touches[0].clientX;
+  handleTouchMove(event: TouchEvent) {
+    if (!this.isDragging) return;
+    const deltaY = event.touches[0].clientX - this.startY;
+    this.target = Math.min(this.maximumX, Math.max(0, this.target - deltaY * 2.5));
+    this.startY = event.touches[0].clientX;
   }
 
-  function handleTouchEnd() {
-    isDragging = false;
+  handleTouchEnd() {
+    this.isDragging = false;
   }
-  smoothScroll()
-  window.addEventListener('wheel', handleWheel,{passive:false})
-  window.addEventListener("mousedown", handleMouseDown);
-  window.addEventListener("mousemove", handleMouseMove);
-  window.addEventListener("mouseup", handleMouseUp);
-  window.addEventListener("touchstart", handleTouchStart);
-  window.addEventListener("touchmove", handleTouchMove);
-  window.addEventListener("touchend", handleTouchEnd);
+
+  init() {
+    this.smoothScroll();
+    window.addEventListener('wheel', this.handleWheel, { passive: false });
+    window.addEventListener('mousedown', this.handleMouseDown);
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+    window.addEventListener('touchstart', this.handleTouchStart);
+    window.addEventListener('touchmove', this.handleTouchMove);
+    window.addEventListener('touchend', this.handleTouchEnd);
+  }
 }
 
-const observer = new MutationObserver(handleSelectionHeaderAppearance);
+const observer = new MutationObserver(() => {
+  new SmoothScroller();
+  const projectThumbnails = document.querySelectorAll('.selection.item-thumbnail')
+  const mainImageThumbnail:HTMLImageElement = document.querySelector('.main-image')!
+
+  const intersectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        console.log(entry.target.getAttribute('src'));
+        mainImageThumbnail.src = entry.target.getAttribute('src')!
+      }
+    })
+  }, {
+    threshold: 0, 
+    rootMargin: `0px -100% 0px 1px`,
+  })
+
+  projectThumbnails.forEach(thumbnail => {
+    intersectionObserver.observe(thumbnail)
+  })
+
+});
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-handleSelectionHeaderAppearance();
+new SmoothScroller();

572c702b30a91e09d30b6197c60db7b1d1bf9ebe
Gathan Mahesa <gthn217@gmail.com>
2023-08-12 15:34:43 +0700

    Smoothscroll on Overview page

--- src/ts/routes/overview.ts
+++ src/ts/routes/overview.ts
@@ -1,11 +1,11 @@
 import projects from '../../../public/works.json';
 
 const overview = `
-<div class="content w-full h-full p-4 flex flex-col gap-16">
-  <header class="grid grid-cols-9 grid-rows-1 text-xs uppercase font-medium leading-4">
+<div class="overview content w-full h-full p-4 flex flex-col gap-16">
+  <header class="grid grid-cols-12 grid-rows-1 text-xs uppercase font-medium leading-4">
     <div>
       <p>OVERVIEW</p>
-      <p>14 Images</p>
+      <p>${projects.length} Images</p>
     </div>
     <div class="col-start-3">
     <p>2018 ━</p>
@@ -18,8 +18,8 @@ const overview = `
         project => `
         <div class="item-container">
         <h1 class="item-title">${project.title}</h1>
-          <img class="item-thumbnail" src="${project.src}" alt="${project.alt}">
-          </div>
+          <img draggable="false" class="item-thumbnail" src="${project.src}" alt="${project.alt}">
+        </div>
       `
       )
       .join('')}
---
+++ src/ts/utils/OverviewScroll.ts
@@ -0,0 +1,96 @@
+export default class OverviewPageSmoothScroll {
+  current: number;
+  target: number;
+  isDragging: boolean;
+  startY: number;
+  ease: number;
+  container: HTMLHeadElement;
+  maximumX: number;
+
+  constructor() {
+    this.current = 0;
+    this.target = 0;
+    this.isDragging = false;
+    this.startY = 0;
+    this.ease = 0.075;
+
+    this.container = document.querySelector('.overview')!;
+    this.maximumX = parseFloat(getComputedStyle(this.container).height) - window.innerHeight * 0.75;
+
+    this.smoothScroll = this.smoothScroll.bind(this);
+    this.handleWheel = this.handleWheel.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleTouchStart = this.handleTouchStart.bind(this);
+    this.handleTouchMove = this.handleTouchMove.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
+
+    this.init();
+  }
+
+  lerp(start: number, end: number, t: number) {
+    return start * (1 - t) + (end * t);
+  }
+
+  setTransform(element: HTMLElement | null, change: string) {
+    if (element) {
+      element.style.transform = change;
+    }
+  }
+
+  smoothScroll() {
+    this.current = this.lerp(this.current, this.target, this.ease);
+    this.current = parseFloat(this.current.toFixed(2));
+    this.setTransform(this.container, `translate3d(0, ${-this.current}px, 0)`);
+    requestAnimationFrame(this.smoothScroll);
+  }
+
+  handleWheel(e: WheelEvent) {
+    e.preventDefault();
+    this.target = Math.min(this.maximumX, Math.max(0, this.target + e.deltaY * 0.5));
+  }
+
+  handleMouseDown(event: MouseEvent) {
+    this.isDragging = true;
+    this.startY = event.clientY;
+  }
+
+  handleMouseMove(event: MouseEvent) {
+    if (!this.isDragging) return;
+    const deltaY = event.clientY - this.startY;
+    this.target = Math.min(this.maximumX, Math.max(0, this.target - deltaY * 2));
+    this.startY = event.clientY;
+  }
+
+  handleMouseUp() {
+    this.isDragging = false;
+  }
+
+  handleTouchStart(event: TouchEvent) {
+    this.isDragging = true;
+    this.startY = event.touches[0].clientY;
+  }
+
+  handleTouchMove(event: TouchEvent) {
+    if (!this.isDragging) return;
+    const deltaY = event.touches[0].clientY - this.startY;
+    this.target = Math.min(this.maximumX, Math.max(0, this.target - deltaY * 2));
+    this.startY = event.touches[0].clientY;
+  }
+
+  handleTouchEnd() {
+    this.isDragging = false;
+  }
+
+  init() {
+    this.smoothScroll();
+    window.addEventListener('wheel', this.handleWheel, { passive: false });
+    window.addEventListener('mousedown', this.handleMouseDown);
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+    window.addEventListener('touchstart', this.handleTouchStart);
+    window.addEventListener('touchmove', this.handleTouchMove);
+    window.addEventListener('touchend', this.handleTouchEnd);
+  }
+}

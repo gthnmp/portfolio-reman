f4fb87a78004f3be8aa258cd23e9f0ca003198f9
Gathan Mahesa <gthn217@gmail.com>
2023-08-12 15:35:40 +0700

    Page transition for about page and little layout adjusments

--- src/ts/routes/about.ts
+++ src/ts/routes/about.ts
@@ -1,25 +1,49 @@
 const about = `
-<div class="content w-screen h-screen flex flex-col gap-16">
+<div class="about content w-screen h-screen flex flex-col gap-16">
   <header class="absolute p-4 grid grid-cols-12 grid-rows-1 uppercase font-medium">
-  <div class="col-span-3 flex flex-col gap-4 leading-4 text-3xl z-50">
-      <p>+62 123 123 123</p>
-      <p>raflydz@gmail.com</p>
+    <div class="col-span-3 flex flex-col leading-4 text-3xl z-50">
+      <div class="line overflow-y-hidden w-max text-3xl">
+        <p>+62 123 123 123</p>
+      </div>
+      <div class="line overflow-y-hidden w-max text-3xl">
+        <p>raflydz@gmail.com</p>
+      </div>
     </div>
     <div class="col-start-9 col-span-4 text-lg uppercase leading-4 grid grid-cols-2 grid-rows-4 gap-y-1">
-      <h1 class="col-start-1">UI / UX Designer</h1> <a class="col-start-2" href="https://www.instagram.com/raflyfdz/">Instagram</a>
-      <h1 class="row-start-3">Freelancer</h1> <a class="col-start-2" href="https://www.linkedin.com/in/rafly-fathurrahman-diaz-822288286/">LinkedIn</a>
-      <a class="col-start-2" href="https://twitter.com/reinmannnt">Twitter</a>
+      <div class="line overflow-y-hidden w-max col-start-1">
+        <h1 class="">UI / UX Designer</h1> 
+      </div>
+      <a href="https://www.instagram.com/raflyfdz/" class="line overflow-y-hidden w-max col-start-2">
+        <div class="">Instagram</div>
+      </a>
+      <div class="line overflow-y-hidden w-max row-start-3">
+        <h1 class="">Freelancer</h1> 
+      </div>
+      <a href="https://www.linkedin.com/in/rafly-fathurrahman-diaz-822288286/" class="line overflow-y-hidden w-max col-start-2">
+        <div class="">LinkedIn</div>
+      </a>
+      <a href="https://twitter.com/reinmannnt" class="line overflow-y-hidden w-max col-start-2">
+        <div class="">Twitter</div>
+      </a>
     </div>
   </header>
-  <main id="main" class="w-screen h-screen grid place-items-center text-6xl text-black ">
-    <div class="flex flex-col items-center justify-center font-medium uppercase">
-      <h1>CATALOGUED</h1>
-      <h1>WORKS</h1>
-      <h1>Rafly Fathurrahman</h1>
-      <h1>C.2023</h1>
+  <main id="main" class="w-screen h-screen grid place-items-center text-6xl text-black">
+    <div class="flex flex-col items-center justify-center font-medium uppercase tracking-wide">
+      <div class="line overflow-y-hidden w-max">
+        <h1>CATALOGUED</h1>
+      </div>
+      <div class="line overflow-y-hidden w-max">
+        <h1>WORKS</h1>
+      </div>
+      <div class="line overflow-y-hidden w-max">
+        <h1>Rafly Fathurrahman</h1>
+      </div>
+      <div class="line overflow-y-hidden w-max">
+        <h1>C.2023</h1>
+      </div>
     </div>
-    </main>
+  </main>
 </div>
-`
+`;
 
-export default about
+export default about;

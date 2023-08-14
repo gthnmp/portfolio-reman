b9531ee94209607217996bc5538d974825e48804
Gathan Mahesa <gthn217@gmail.com>
2023-08-14 23:17:22 +0700

    remove unused codes

--- src/ts/utils/gl/renderer.ts
+++ src/ts/utils/gl/renderer.ts
@@ -1,13 +1,13 @@
-  function render(gl : WebGLRenderingContext, program: WebGLProgram, texture:WebGLTexture) {
-    const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution')
-    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
+function render(gl : WebGLRenderingContext, program: WebGLProgram, texture:WebGLTexture) {
+  const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution')
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
 
-    gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    var primitiveType = gl.TRIANGLES;
-    var new_offset = 0;
-    var count = 6;
-    gl.drawArrays(primitiveType, new_offset, count)
-  }
+  var primitiveType = gl.TRIANGLES;
+  var new_offset = 0;
+  var count = 6;
+  gl.drawArrays(primitiveType, new_offset, count)
+}
 
-  export default render
+export default render
